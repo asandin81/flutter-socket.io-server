6441ebbdf5d6b335626f1dc51ee9bfb41ba045fe
Angel Sandin Sanchez <angel@sandin.cat>
2021-06-06 09:02:33 +0200

    Fin seccion 3

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+// Servidor socket
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+
+require('./sockets/socket');
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (error) => {
+    if (error) throw new Error(error);
+
+    console.log('Servidor correindo en puerto: ', process.env.PORT);
+});
